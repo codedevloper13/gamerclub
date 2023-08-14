e2134aa4447eb1e447158df73c3688ac089c6227
codedevloper13 <codedevloper13@gmail.com>
2023-08-14 17:42:40 +0530

    Changes and global poppins and monserrat font adding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,18 @@
-import type { Config } from 'tailwindcss'
+/** @format */
+
+import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ["var(--font-poppins)", ...fontFamily],
+				montserrat: ["var(--font-montserrat)", ...fontFamily],
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
